a260ff4949f6574f0e303bf63703e49f4f2737de
2099492 <2099492@talnet.com>
2019-11-15 13:32:58 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hoe oud ben je?" . PHP_EOL;
+$age1 = readline();
+$age2 = 16.5;
+if ($age1 > $age2) {
+  echo "Je mag beginnen met rijlessen!";
+} else {
+  echo "Helaas, je mag nog niet beginnen met rijlessen";
+}
+
+ ?>
